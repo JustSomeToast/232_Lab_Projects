6ec7f4d4e1384ded22c8a2db3aef2084e7118ce4
Tyler Gentry <tgentry2753@gmail.com>
2018-03-01 16:31:57 -0700

    Not the first Git command of all time

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * Property of no one in particular. Open Project Properties to change this.
+ * 
+ */
+
+package helloworld;
+
+/**
+ *
+ * @author Tyler
+ */
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+    }
+    
+}
